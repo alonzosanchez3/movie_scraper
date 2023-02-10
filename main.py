47cfb33bede0160b1a7b5efb94fbf0cf7b47d56c
Alonzo Sanchez <alonzosanchez3@gmail.com>
2023-02-10 10:45:23 -0600

    created soup from website html

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ from bs4 import BeautifulSoup
 import requests
 
 MOVIE_URL = 'https://www.empireonline.com/movies/features/best-movies-2/'
+
+response = requests.get(url=MOVIE_URL)
+soup = BeautifulSoup(response.text, 'html.parser')
