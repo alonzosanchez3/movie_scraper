f6aca53eaa9fb6dc263f0534096fe315c09a205f
Alonzo Sanchez <alonzosanchez3@gmail.com>
2023-02-11 10:45:23 -0600

    getting all movie titles

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ MOVIE_URL = 'https://www.empireonline.com/movies/features/best-movies-2/'
 
 response = requests.get(url=MOVIE_URL)
 soup = BeautifulSoup(response.text, 'html.parser')
+h3_class = 'listicleItem_listicle-item__title__hW_Kn'
+movie_titles = soup.find_all(name='h3', class_=h3_class)
+movie_titles = [movie.getText() for movie in movie_titles]
+print(movie_titles)
