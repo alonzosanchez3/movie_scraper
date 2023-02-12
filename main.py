8e276ff67b0f175b6c4908eaa45579baf94f2e3b
Alonzo Sanchez <alonzosanchez3@gmail.com>
2023-02-12 10:45:23 -0600

    pushed all movies to a file txt

--- main.py
+++ main.py
@@ -8,4 +8,9 @@ soup = BeautifulSoup(response.text, 'html.parser')
 h3_class = 'listicleItem_listicle-item__title__hW_Kn'
 movie_titles = soup.find_all(name='h3', class_=h3_class)
 movie_titles = [movie.getText() for movie in movie_titles]
+movie_titles = movie_titles[::-1]
 print(movie_titles)
+
+with open('movies.txt', 'w') as file:
+  for movie in movie_titles:
+    file.write(f"{movie}\n")
